a5cae9d52632abb9254be067bf85edd14e6ffbb5
int22 <int22@users.noreply.github.com>
2017-08-11 17:19:18 -0500

    Update report.php

--- report.php
+++ report.php
@@ -34,9 +34,9 @@ if (isset($_GET['from']) && isset($_GET['players']) && isset($_GET['reason']) &&
     $ch = curl_init($url);
     
     curl_setopt_array($ch, array(
-	      CURLOPT_RETURNTRANSFER => 1, // Block echoing of response
+	CURLOPT_RETURNTRANSFER => 1, // Block echoing of response
         CURLOPT_SSL_VERIFYPEER => false,
-     	  CURLOPT_CAINFO => getcwd() . '/discord.crt',
+	CURLOPT_CAINFO => getcwd() . '/discord.crt',
         CURLOPT_POSTFIELDS => $payload,
         CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8')
     ));
